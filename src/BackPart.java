44e0fb2f8b9dcd0316a3d57c76a67fe64295fe7d
Jacek <ja77co@wp.pl>
2019-06-03 20:15:02 +0200

    Lepiej nie będzie

--- src/BackPart.java
+++ src/BackPart.java
@@ -1,25 +1,18 @@
 import java.util.Arrays;
-import java.util.Random;
 
 public class BackPart {
     public static void main(String[] args) {
-        int[] arrayIn = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arrayIn = {1,2,3,4,5,6,7,8,9,};
+        int number = 7;
+        System.out.println(Arrays.toString(arrayIn));
 
-        //int[] arrayIn = new int[10];
-        //Random random = new Random();
-        //for (int i = 0; i < arrayIn.length; i++){
-        //    arrayIn[i] = random.nextInt(99);
-        //}
 
-        String arrayInString = Arrays.toString(arrayIn);
-        System.out.println(arrayInString);
-
-        String newArrayString = Arrays.toString(changing(arrayIn, 5));
-        System.out.println(newArrayString);
+        if (arrayIn.length == 0 || number > arrayIn.length) {
+            System.out.println("Błedne dane");
+        } else System.out.println(Arrays.toString(changedArray(arrayIn, number)));
     }
 
-    static int[] changing(int[] arrayIn, int number) {
-
+    static int[] changedArray(int[] arrayIn, int number) {
         int[] newArray = new int[arrayIn.length];
         for (int i = 0; i < number; i++) {
             newArray[i] = arrayIn[number - 1 - i];
